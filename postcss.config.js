100b548401eb7e3210532a3688bf6a530ac78e3f
wrakc <carlosveiga1618@gmail.com>
2020-10-16 16:52:20 -0300

    add tailwindcss to project

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-module.exports = {
+let environment = {
   plugins: [
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -10,3 +11,5 @@ module.exports = {
     })
   ]
 }
+
+module.exports = environment;
