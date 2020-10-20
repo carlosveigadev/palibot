055cd8d5bbc4397702925f3f4a29d1698385f24e
wrakc <carlosveiga1618@gmail.com>
2020-10-20 19:12:54 -0300

    fix most voted article not working properly

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :article
   belongs_to :user
 
-  scope :most_voted, -> { group(:article_id).order('article_id DESC').count }
+  scope :most_voted, -> { group('article_id').count }
 end
