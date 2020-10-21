060880a4e8d89a8d313b5afd33a2215b56a96c92
wrakc <carlosveiga1618@gmail.com>
2020-10-21 14:26:01 -0300

    add category show page and proper routing
    fix controller to display articles inside category controller

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,6 +1,6 @@
 class CategoryController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @articles = @category.articles.order(created_at: :desc).includes(:username)
+    @articles = @category.articles.order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :articles do
     resources :votes
   end
+  resources :category, only: [:show]
 end
