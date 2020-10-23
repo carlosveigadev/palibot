35a472c0a5bb281f2254284958d4be31d240b4db
wrakc <carlosveiga1618@gmail.com>
2020-10-23 14:24:01 -0300

    add AS33 config

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'faker'
 
 gem 'inline_svg'
 
+gem 'aws-sdk'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create name: 'North America', priority: 1
+Category.create name: 'South America', priority: 2
+Category.create name: 'Central America', priority: 3
+Category.create name: 'Caribbean', priority: 4
+Category.create name: 'Africa', priority: 5
+Category.create name: 'Middle East', priority: 6
+Category.create name: 'Europe', priority: 7
+Category.create name: 'Asia', priority: 8
+Category.create name: 'Australia & Oceania', priority: 9
+Category.create name: 'Antarctica', priority: 10
+fry = User.create username: 'Fry', email: 'fry@futurama.com', password: '123456', password_confirmation: '123456'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '1'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '2'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '3'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '4'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '5'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '6'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '7'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '8'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '9'
+article1 = fry.article.create title: 'This is the first main file', content: 'First article to populate the DB', image: File.new(Rails.root.join('public', 'test_image.png')), category_ids: '10'
+
