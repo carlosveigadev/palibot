792634e5479218a763c116484cf6981d62b7ff81
wrakc <carlosveiga1618@gmail.com>
2020-10-28 13:01:33 -0300

    fix rubocop offenses

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Session Authentication', type: :feature do
   before :each do
-  User.create(username: 'bender', password: '123456', email: 'bender@futurama.com') 
-  Category.create(id: 1, name: 'test', priority: 1) 
-  user.articles.new(id: 1,
-                    title: 'Title',
-                    content: 'Anything',
-                    created_at: DateTime.now,
-                    updated_at: DateTime.now,
-                    image: File.new(Rails.root.join('public', 'test_image.png')),
-                    user_id: 1,
-                    category_id: 1) 
+    User.create(username: 'bender', password: '123456', email: 'bender@futurama.com')
+    Category.create(id: 1, name: 'test', priority: 1)
+    user.articles.new(id: 1,
+                      title: 'Title',
+                      content: 'Anything',
+                      created_at: DateTime.now,
+                      updated_at: DateTime.now,
+                      image: File.new(Rails.root.join('public', 'test_image.png')),
+                      user_id: 1,
+                      category_id: 1)
 
     scenario 'Log in' do
       visit new_user_session_path
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,81 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'Sign up', type: :feature do
   before :each do
-    User.create(username: 'bender', password: '123456', email: 'bender@futurama.com') 
-    Category.create(id: 1, name: 'test', priority: 1) 
-    user.articles.new(id: 1,
-                      title: 'Title',
-                      content: 'Anything',
-                      created_at: DateTime.now,
-                      updated_at: DateTime.now,
-                      image: File.new(Rails.root.join('public', 'test_image.png')),
-                      user_id: 1,
-                      category_id: 1) 
+    User.create(username: 'bender', password: '123456', email: 'bender@futurama.com')
+    Category.create(id: 1, name: 'test', priority: 1)
+    user.articles.new(id: 1, title: 'Title', content: 'Anything', created_at: DateTime.now,
+                      updated_at: DateTime.now, image: File.new(Rails.root.join('public', 'test_image.png')),
+                      user_id: 1, category_id: 1)
 
-  scenario 'Sign up' do
-    visit new_user_registration_path
+    scenario 'Sign up' do
+      visit new_user_registration_path
+      fill_in 'user[username]', with: 'bender'
+      fill_in 'user[email]', with: 'bender@futurama.com'
+      fill_in 'user[password]', with: '123456'
+      fill_in 'user[password_confirmation]', with: '123456'
+      click_button 'Sign Up'
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
 
-    fill_in 'user[username]', with: 'bender'
-    fill_in 'user[email]', with: 'bender@futurama.com'
-    fill_in 'user[password]', with: '123456'
-    fill_in 'user[password_confirmation]', with: '123456'
-
-    click_button 'Sign Up'
-
-    expect(page).to have_current_path(root_path)
-    expect(page).to have_content('Welcome! You have signed up successfully.')
+    scenario 'Pasword confirmation does not match' do
+      visit new_user_registration_path
+      fill_in 'user[username]', with: 'test'
+      fill_in 'user[email]', with: 'test@test.com'
+      fill_in 'user[password]', with: '123456'
+      fill_in 'user[password_confirmation]', with: '12345'
+      click_button 'Sign Up'
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
   end
-
-  scenario 'User Already Exists' do
-    visit new_user_registration_path
-
-    fill_in 'user[username]', with: user.username
-    fill_in 'user[email]', with: 'test@test.com'
-    fill_in 'user[password]', with: '123456'
-    fill_in 'user[password_confirmation]', with: '123456'
-
-    click_button 'Sign Up'
-
-    expect(page).to have_content('Username has already been taken')
-  end
-
-  scenario 'Email Already Exists' do
-    visit new_user_registration_path
-
-    fill_in 'user[username]', with: 'test'
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: '123456'
-    fill_in 'user[password_confirmation]', with: '123456'
-
-    click_button 'Sign Up'
-
-    expect(page).to have_content('Email has already been taken')
-  end
-
-  scenario 'Pasword confirmation does not match' do
-    visit new_user_registration_path
-
-    fill_in 'user[username]', with: 'test'
-    fill_in 'user[email]', with: 'test@test.com'
-    fill_in 'user[password]', with: '123456'
-    fill_in 'user[password_confirmation]', with: '12345'
-
-    click_button 'Sign Up'
-
-    expect(page).to have_content("Password confirmation doesn't match Password")
-  end
-
-  scenario "Username can't be blank" do
-    visit new_user_registration_path
-
-    fill_in 'user[username]', with: ''
-    fill_in 'user[email]', with: 'test@test.com'
-    fill_in 'user[password]', with: '123456'
-    fill_in 'user[password_confirmation]', with: '123456'
-
-    click_button 'Sign Up'
-
-    expect(page).to have_content("Username can't be blank")
-  end
-end
 end
